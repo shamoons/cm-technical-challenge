b06aaa53fba19a8e9c6324ea8927b664b4821632
Shamoon Siddiqui <shamoon@develop.io>
2022-11-23 12:44:09 -0500

    Wooo hoooo

--- handler.js
+++ handler.js
@@ -1,1 +1,11 @@
-// write your lambda here
+export const getHandler = async (event, context) => {
+  const reversedPayload = event.body.split('').reverse().join('');
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Thank you for the reverser!',
+      payload: reversedPayload
+    })
+  }
+}
