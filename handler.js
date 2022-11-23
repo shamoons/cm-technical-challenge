dba1d95074721dc4485d8d087911e6978bba10ec
Shamoon Siddiqui <shamoon@develop.io>
2022-11-23 12:46:32 -0500

    Keep it simple

--- handler.js
+++ handler.js
@@ -1,11 +1,13 @@
-export const reverser = async (event, context) => {
-  const reversedPayload = event.body.split('').reverse().join('');
+module.exports = {
+  reverser: async (event, context) => {
+    const reversedPayload = event.body.split('').reverse().join('');
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Thank you for the reverser!',
-      payload: reversedPayload
-    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Thank you for the reverser!',
+        payload: reversedPayload
+      })
+    }
   }
 }
